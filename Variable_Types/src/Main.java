e279d9edfac0f73d84052d95d72aa0c0078dfdfa
Yusuf Cem ŞEN <84101339+YusufCemSen@users.noreply.github.com>
2023-07-05 08:07:49 +0300

    Add files via upload

---
+++ Variable_Types/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte myByte=126;
+        short myShort=327;
+        int myInt=1058;
+        long myLong=12042;
+        float myFloat= 42.8f;
+        double myDouble=8.58;
+        char myChar='A';
+        boolean myBoolean=true;
+
+
+        String myText="Merhaba !";
+
+
+        System.out.println(myText+"\nByte Değeri:"+myByte+"\nShort Değeri:"+myShort+"\nInteger Değeri:"+myInt+"\nLong Değeri:"+ myLong+"\nFloat Değeri:"+myFloat+"\nDouble Değeri:"+myDouble+"\nChar Değeri:"+myChar+"\nBoolean Değeri:"+myBoolean);
+
+    }
+}
